631cc4d88806d960375f410cf5ab9d2f7a7d9d4f
RyanSweden03 <89040278+RyanSweden03@users.noreply.github.com>
2022-09-27 23:50:13 -0500

    feat: Modified Products component.

--- src/router.js
+++ src/router.js
@@ -2,14 +2,14 @@ import ProductsCategoryComponent from "./products/pages/products-category.compon
 import { createRouter, createWebHistory } from "vue-router"; //Vue vite
 import Dogproducts from "./products/pages/products-list.component.vue";
 const routes = [
-    {path: '/', component: ProductsCategoryComponent},
-    {path: "/products", component: ProductsCategoryComponent},
-    {path: "/products/:category", component: Dogproducts},
+  { path: "/", component: ProductsCategoryComponent },
+  { path: "/products", component: ProductsCategoryComponent },
+  { path: "/products/:category", component: Dogproducts },
 ];
 
 const history = createWebHistory();
 const router = createRouter({
-    history,
-    routes
-})
+  history,
+  routes,
+});
 export default router;
