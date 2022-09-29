99020e9a0f91e6129929beb21d1d53be8f4fbc14
GabrielGomezDlc <u202020341@upc.edu.pe>
2022-09-29 03:31:25 -0500

    feat(HomePetOwner): Structure for home-pet-owner component and information in db.json added

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import ProductsCategoryComponent from "./products/pages/products-category.component.vue";
 import { createRouter, createWebHistory } from "vue-router"; //Vue vite
 import Dogproducts from "./products/pages/products-list.component.vue";
+
 const routes = [
   { path: "/", component: ProductsCategoryComponent },
   { path: "/products", component: ProductsCategoryComponent },
