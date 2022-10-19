e1fad2d210fcec61e783a5c92d75f602afccccec
Asura <u202020239@upc.edu.pe>
2022-10-18 21:49:40 -0500

    feat(component) Add profile component

--- src/profile/pages/profile.component.vue
+++ src/profile/pages/profile.component.vue
@@ -1,14 +1,108 @@
 <template>
+  <div class="myprofile-container">
+    <div class="container-body">
 
+      <div class="card" >
+        <div class="card-content" v-for="profile in profile" :key="profile.id">
+          <div class="container-title">
+            <div clas="container-title-image">
+              <img alt="profile" :src="profile.photoUrl"/>
+            </div>
+            <div class="container-title-name">
+              <h1>{{profile.name}}</h1>
+            </div>
+          </div>
+          <div class="container-body-text">
+            <div class="icon">
+              <i class="pi pi-calendar"></i>
+              <p>{{profile.birthday}}</p>
+            </div>
+            <div class="icon">
+              <i class="pi pi-phone"></i>
+              <p>{{profile.phone}}</p>
+            </div>
+            <div class="icon">
+              <i class="pi pi-envelope"></i>
+              <p>{{profile.email}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import {ProfileServices} from "../services/profile.services.js";
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Profile'
+  name:'Profile',
+  data(){
+    return{
+      profile:null,
+
+    }
+  },
+  created(){
+    new ProfileServices().getProfile().then(response => {
+      this.profile = response.data
+    })
+  },
 }
 </script>
 
+
 <style scoped>
+*{
+  font-family: 'IBM Plex Sans',sans-serif;
+  font-size: 20px;
+}
+
+.myprofile-container{
+  width: 97%;
+  height: 100%;
+  border-radius: 20px;
+  padding: 16px;
+  margin-top: 20px;
+  margin-left: 10px;
+  background: #f5f5f5;
+}
+
+.container-title{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+img{
+  display: flex;
+  border-radius: 400%;
+  width: 320px;
+  height: 400px;
+  margin-top: 2%;
+}
+
+.container-title-name{
+  margin-top: 1%;
+}
+
+.container-body-text{
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  margin: 2%;
+}
+
+.icon{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 1px;
+}
+
+i{
+  margin: 2%;
+}
 
 </style>
