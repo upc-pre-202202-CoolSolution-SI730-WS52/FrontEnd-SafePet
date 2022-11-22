c1b3bc3624059410a34be1b404a05a7f8fb22149
GabrielGomezDlc <u202020341@upc.edu.pe>
2022-11-21 23:55:00 -0500

    feat(services):modified services

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -2,10 +2,8 @@ import axios from "axios";
 
 export default axios.create({
 
-  //baseURL: "https://safepet-fake-api.herokuapp.com",
-  baseURL: "https://localhost:7283/api/v1",
+  baseURL: "https://safepet-web-services.herokuapp.com/api/v1",
   headers: { "Content-type": "application/json","authorization":`Bearer ${String(sessionStorage.getItem("JWT"))}`},
-
 
 });
 
